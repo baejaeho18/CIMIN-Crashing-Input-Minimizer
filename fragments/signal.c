e4e265da2cafa52c7e8344553c67903bc6c736dd
Narin Han <lynniepro21@gmail.com>
2023-04-04 18:22:50 +0900

    add fragments/signal.c

---
+++ fragments/signal.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+void 
+handler_sigint(int sig) 
+{
+    if (sig == SIGINT) { // ctrl + c pressed
+        int temp = 99;
+        printf("\n%d\n", temp); // prints out the size of the shortest crashing input found so far
+        // TODO : produces output file
+        exit(0); // terminate the execution
+    }
+
+}
+
+void 
+handler_sigalrm(int sig) 
+{ 
+    // kill process 
+    pid_t cur;
+    cur = getpid();
+    kill(cur, SIGTERM);
+}
+
+int 
+main() 
+{
+    // signal(SIGINT, handler_sigint);
+    
+    signal(SIGALRM, handler_sigalrm);
+
+    struct itimerval t;
+
+    t.it_value.tv_sec = 3;
+    t.it_interval = t.it_value;
+
+    setitimer(ITIMER_REAL, &t, 0x0);
+
+    while(1);
+}
