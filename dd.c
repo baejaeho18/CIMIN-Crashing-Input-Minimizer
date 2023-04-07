0358023445a507ca3df04ef33551d8645297d6f2
baejae <37645490+baejaeho18@users.noreply.github.com>
2023-04-07 23:10:07 +0900

    Update dd.c

--- dd.c
+++ dd.c
@@ -7,81 +7,82 @@ reduce(char* original_input)
 {
     int input_size = strlen(original_input) - 1;
 
-    char* reduced_input = (char*)malloc(sizeof(char) * input_size);
+    char* reduced_input;
     char* error_message = (char*)malloc(sizeof(char) * input_size);
     char* error_keyword = (char*)malloc(sizeof(char) * 10);         //strlen(keyword)
 
     while(input_size > 0)
     {
-        unsigned int stop;
-        for(int i=0;i<strlen(original_input) - input_size; i++)
+        printf("%d\n", input_size);
+        for(int i=0; i<=strlen(original_input) - input_size; i++)
         {
             char* head;
             char* tail;
             if(0 > i-1)
                 head = "";
             else{
-                head = (char*)malloc(sizeof(char) * (i+1));
+                head = (char*)malloc(sizeof(char) * i);
                 strncpy(head, original_input, i);
             }
-            if(i+input_size > input_size)
+            if(i+input_size > strlen(original_input)-1)
                 tail = "";
             else{    // i = 0
-                tail = (char*)malloc(sizeof(char) * (2-i));
-                strncpy(tail, original_input+i+input_size, 1-i);
+                tail = (char*)malloc(sizeof(char) * (strlen(original_input)-i-input_size));
+                strncpy(tail, original_input+i+input_size, strlen(original_input)-i-input_size);
             }
 
             //for test
-            printf("head: %s, tail: %s\n", head, tail);
+            printf("(%d) head: %s, tail: %s\n", i, head, tail);
 
             // fork - pipe - write(head+tail);
-            
+            reduced_input = (char*)malloc(sizeof(char) * (strlen(original_input)-input_size-1));
             strcat(reduced_input, head);
             strcat(reduced_input, tail);
+            free(head);
+            free(tail);
 
             // for test
-            printf("reduced input:%s\n", reduced_input);
+            printf("(%d) reduced input:%s\n", i, reduced_input);
 
             // read(error_message)
-            if(strstr(error_message, error_keyword) != NULL)
-                break;
-            else
-                reduce(reduced_input);
+            // if(strstr(error_message, error_keyword) != NULL)
+            //     break;
+            // else
+            //     reduce(reduced_input);
         }
-        for(int i=0; i<strlen(original_input)-input_size; i++)
+        for(int i=0; i<=strlen(original_input)-input_size; i++)
         {
             char* mid;
-            if(i > i + input_size - 1)
-                mid = "";
-            else if(i+input_size-1 > input_size)
+            if(i > i+input_size-1)
             {
-                mid = (char*)malloc(sizeof(char) * (input_size + 2 - i));
-                strncpy(mid, original_input+i, input_size + 1 - i);
+                mid = "";
             }
             else
             {
-                mid = (char*)malloc(sizeof(char) * (input_size + 1));
+                mid = (char*)malloc(sizeof(char) * input_size);
                 strncpy(mid, original_input+i, input_size);
             }
-            printf("mid:%s\n", mid);
+            printf("(%d) mid:%s\n", i, mid);
 
             // fork - pipe - write(mid)
             // read(error_message)
-
-            if(strstr(error_message, error_keyword) != NULL)
-                break;
-            else
-                reduce(mid);
+            free(mid);
+            // if(strstr(error_message, error_keyword) != NULL)
+            //     break;
+            // else
+            //     reduce(mid);
         }
         input_size = input_size - 1;
     }
-
+    free(reduced_input);
+    free(error_keyword);
+    free(error_message);
     return original_input;
 }
 
 int
 main()
 {
-    char* crashing_input = "abcdef";
+    char* crashing_input = "abcde";
     reduce(crashing_input);
 }
